6b51a9bf05db17fce53fae2b1c5da453350924e2
Jaroslav Bažant <jardabazant@seznam.cz>
2017-12-18 14:32:23 +0100

    Initial Commit

---
+++ AG1_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AG1_2
+//
+//  Created by Jaroslav Bažant on 18.12.17.
+//  Copyright © 2017 DewDrops. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
